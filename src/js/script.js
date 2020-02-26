26763be1fb806533cda3ef0fd0a9a5a28056093d
Raphael <raphael.ferreira@hetic.net>
2020-02-26 15:48:26 +0100

    adding cursor and mixins

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,25 @@
+//intro
+
 setTimeout(() => {
   let intro = document.querySelector(".container");
   intro.classList.add("container--visible");
 }, 4500);
+
+//cursor
+
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", e => {
+  cursor.setAttribute(
+    "style",
+    "top: " + (e.pageY - 15) + "px; left: " + (e.pageX - 15) + "px;"
+  );
+});
+
+document.addEventListener("click", () => {
+  cursor.classList.add("expand");
+
+  setTimeout(() => {
+    cursor.classList.remove("expand");
+  }, 500);
+});
