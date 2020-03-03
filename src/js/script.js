53d1afcf8d59c0c0c28ca3cb9d335b64e9b38314
Raphael <raphael.ferreira@hetic.net>
2020-03-03 09:50:55 +0100

    change timing for projects animation

--- src/js/script.js
+++ src/js/script.js
@@ -31,7 +31,7 @@ let projects = document.querySelectorAll(".project");
 
 for (let i = 0; i < projects.length; i++) {
   window.addEventListener("scroll", () => {
-    if (window.scrollY + window.innerHeight / 2 >= projects[i].offsetTop) {
+    if (window.scrollY + window.innerHeight * 0.75 >= projects[i].offsetTop) {
       projects[i].classList.add("project--reached");
     }
   });
