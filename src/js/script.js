71112d20004d44c14c882c18bdf6ea08882ee935
Raphael <raphael.ferreira@hetic.net>
2020-03-13 15:05:21 +0100

    animation on about page

--- src/js/script.js
+++ src/js/script.js
@@ -26,13 +26,3 @@ document.addEventListener("click", () => {
     cursor.classList.remove("expand");
   }, 500);
 });
-
-let projects = document.querySelectorAll(".project");
-
-for (let i = 0; i < projects.length; i++) {
-  window.addEventListener("scroll", () => {
-    if (window.scrollY + window.innerHeight * 0.75 >= projects[i].offsetTop) {
-      projects[i].classList.add("project--reached");
-    }
-  });
-}
---
+++ src/js/scroll.js
@@ -0,0 +1,24 @@
+let projects = document.querySelectorAll(".project");
+
+for (let i = 0; i < projects.length; i++) {
+  window.addEventListener("scroll", () => {
+    if (window.scrollY + window.innerHeight * 0.75 >= projects[i].offsetTop) {
+      projects[i].classList.add("project--reached");
+    }
+  });
+}
+
+const photo = document.querySelector(".presentation__photo");
+const texts = document.querySelectorAll(".presentation__lines");
+
+window.addEventListener("scroll", () => {
+  let scrollValue = window.pageYOffset;
+  photo.style.transform = "rotate(" + scrollValue / 15 + "deg)";
+  texts.forEach((text, index) => {
+    if (index % 2 === 0) {
+      text.style.transform = `translateX(${scrollValue / 10}px)`;
+    } else {
+      text.style.transform = `translateX(-${scrollValue / 10}px)`;
+    }
+  });
+});
