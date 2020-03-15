9b2220dc5fe0cd3752d82dbef3dcc440e12315ff
Raphael <raphael.ferreira@hetic.net>
2020-03-15 17:18:35 +0100

    better js on scroll for index

--- src/js/index-scroll.js
+++ src/js/index-scroll.js
@@ -1,9 +1,9 @@
 let projects = document.querySelectorAll(".project");
 
-for (let i = 0; i < projects.length; i++) {
-  window.addEventListener("scroll", () => {
-    if (window.scrollY + window.innerHeight * 0.75 >= projects[i].offsetTop) {
-      projects[i].classList.add("project--reached");
+window.addEventListener("scroll", () => {
+  projects.forEach(project => {
+    if (window.scrollY + window.innerHeight * 0.75 >= project.offsetTop) {
+      project.classList.add("project--reached");
     }
   });
-}
+});
