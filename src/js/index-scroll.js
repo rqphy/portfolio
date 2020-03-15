aaf4c660ca4e06dfa8eb159ffa852b1f3cb59853
Raphael <raphael.ferreira@hetic.net>
2020-03-15 17:29:46 +0100

    change index animation on scroll

--- src/js/index-scroll.js
+++ src/js/index-scroll.js
@@ -1,9 +1,12 @@
-let projects = document.querySelectorAll(".project");
+const projects = document.querySelectorAll(".project");
 
 window.addEventListener("scroll", () => {
+  let scrollValue = window.pageYOffset;
   projects.forEach(project => {
-    if (window.scrollY + window.innerHeight * 0.75 >= project.offsetTop) {
-      project.classList.add("project--reached");
-    }
+    // if (window.scrollY + window.innerHeight * 0.75 >= project.offsetTop) {
+    //   project.classList.add("project--reached");
+    // }
+    project.style.transform = `skewX(${-(scrollValue - project.offsetTop) /
+      30}deg)`;
   });
 });
