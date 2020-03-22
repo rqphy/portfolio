79f2bd7f6ca299bfe74f6d3801cb77aa03530d8f
Raphael <raphael.ferreira@hetic.net>
2020-03-22 19:28:39 +0100

    change transitions on about page

--- src/js/about-scroll.js
+++ src/js/about-scroll.js
@@ -4,7 +4,7 @@ const pitch = document.querySelector(".spitch__text");
 
 window.addEventListener("scroll", () => {
   let scrollValue = window.pageYOffset;
-  photo.style.transform = "rotate(" + scrollValue / 50 + "deg)";
+  photo.style.transform = "rotate(-" + scrollValue / 80 + "deg)";
   texts.forEach((text, index) => {
     if (index % 2 === 0) {
       text.style.transform = `translateX(${scrollValue / 10 - 100}px)`;
@@ -12,5 +12,5 @@ window.addEventListener("scroll", () => {
       text.style.transform = `translateX(${-scrollValue / 10 - 70}px)`;
     }
   });
-  pitch.style.transform = `skewX(-${scrollValue / 50}deg)`;
+  pitch.style.transform = `skewX(-${scrollValue / 80}deg)`;
 });
