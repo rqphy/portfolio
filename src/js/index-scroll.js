90a6ffba0911c35ed247981874447b6fab2b0c41
Raphael <raphael.ferreira@hetic.net>
2020-03-25 20:16:29 +0100

    slow animation on projects

--- src/js/index-scroll.js
+++ src/js/index-scroll.js
@@ -3,10 +3,7 @@ const projects = document.querySelectorAll(".project");
 window.addEventListener("scroll", () => {
   let scrollValue = window.pageYOffset;
   projects.forEach(project => {
-    // if (window.scrollY + window.innerHeight * 0.75 >= project.offsetTop) {
-    //   project.classList.add("project--reached");
-    // }
     project.style.transform = `skewX(${-(scrollValue - project.offsetTop) /
-      30}deg)`;
+      50}deg)`;
   });
 });
